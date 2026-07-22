ccb2b9f165b1ae6867f231a9d703f60566c930eb
Luis Otavio <luisotv.bim@gmail.com>
2026-07-22 10:25:44 -0300

    InsertNotes: checkboxes horizontais empilhadas por nivel, selecao unica por linha

--- PluginConceito/03-Modules/InsertNotes/UI/DisciplineViewModel.cs
+++ PluginConceito/03-Modules/InsertNotes/UI/DisciplineViewModel.cs
@@ -1,18 +1,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Windows;
 
 namespace PluginConceito.Modules.InsertNotes
 {
     internal sealed class DisciplineViewModel : INotifyPropertyChanged
     {
         private bool _isChecked;
-        private bool _isVisible;
 
         public string Name { get; }
-
-        public int Level { get; }
 
         public List<DisciplineViewModel> Children { get; } = new List<DisciplineViewModel>();
 
@@ -28,50 +24,12 @@ namespace PluginConceito.Modules.InsertNotes
 
                 _isChecked = value;
                 OnPropertyChanged();
-
-                foreach (DisciplineViewModel child in Children)
-                {
-                    child.IsVisible = value;
-                }
             }
         }
 
-        public bool IsVisible
-        {
-            get { return _isVisible; }
-            set
-            {
-                if (_isVisible == value)
-                {
-                    return;
-                }
-
-                _isVisible = value;
-                OnPropertyChanged();
-
-                if (!value)
-                {
-                    _isChecked = false;
-                    OnPropertyChanged(nameof(IsChecked));
-
-                    foreach (DisciplineViewModel child in Children)
-                    {
-                        child.IsVisible = false;
-                    }
-                }
-            }
-        }
-
-        public Thickness IndentMargin
-        {
-            get { return new Thickness(Level * 24, 2, 0, 2); }
-        }
-
-        public DisciplineViewModel(string name, int level)
+        public DisciplineViewModel(string name)
         {
             Name = name;
-            Level = level;
-            _isVisible = level == 0;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
--- PluginConceito/03-Modules/InsertNotes/UI/InsertNotesViewModel.cs
+++ PluginConceito/03-Modules/InsertNotes/UI/InsertNotesViewModel.cs
@@ -1,53 +1,184 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace PluginConceito.Modules.InsertNotes
 {
-    internal sealed class InsertNotesViewModel
+    internal sealed class InsertNotesViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<DisciplineViewModel> AllDisciplines { get; }
+        private bool _isUpdating;
+        private bool _hasLevel1;
+        private bool _hasLevel2;
+
+        public ObservableCollection<DisciplineViewModel> Level0 { get; } = new ObservableCollection<DisciplineViewModel>();
+        public ObservableCollection<DisciplineViewModel> Level1 { get; } = new ObservableCollection<DisciplineViewModel>();
+        public ObservableCollection<DisciplineViewModel> Level2 { get; } = new ObservableCollection<DisciplineViewModel>();
+
+        public bool HasLevel1
+        {
+            get { return _hasLevel1; }
+            private set { _hasLevel1 = value; OnPropertyChanged(); }
+        }
+
+        public bool HasLevel2
+        {
+            get { return _hasLevel2; }
+            private set { _hasLevel2 = value; OnPropertyChanged(); }
+        }
 
         public InsertNotesViewModel()
         {
-            List<DisciplineViewModel> root = BuildTree();
-            AllDisciplines = new ObservableCollection<DisciplineViewModel>(Flatten(root));
+            List<DisciplineViewModel> tree = BuildTree();
+
+            foreach (DisciplineViewModel item in tree)
+            {
+                Level0.Add(item);
+                WireUp(item);
+            }
+        }
+
+        private void WireUp(DisciplineViewModel item)
+        {
+            item.PropertyChanged += OnDisciplinePropertyChanged;
+
+            foreach (DisciplineViewModel child in item.Children)
+            {
+                WireUp(child);
+            }
+        }
+
+        private void OnDisciplinePropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName != nameof(DisciplineViewModel.IsChecked))
+            {
+                return;
+            }
+
+            if (_isUpdating)
+            {
+                return;
+            }
+
+            var discipline = (DisciplineViewModel)sender;
+
+            if (Level0.Contains(discipline))
+            {
+                if (discipline.IsChecked)
+                {
+                    _isUpdating = true;
+
+                    foreach (DisciplineViewModel sibling in Level0)
+                    {
+                        if (sibling != discipline)
+                        {
+                            sibling.IsChecked = false;
+                        }
+                    }
+
+                    _isUpdating = false;
+
+                    Level1.Clear();
+                    Level2.Clear();
+                    HasLevel1 = false;
+                    HasLevel2 = false;
+
+                    foreach (DisciplineViewModel child in discipline.Children)
+                    {
+                        Level1.Add(child);
+                    }
+
+                    HasLevel1 = Level1.Count > 0;
+                }
+                else
+                {
+                    Level1.Clear();
+                    Level2.Clear();
+                    HasLevel1 = false;
+                    HasLevel2 = false;
+                }
+            }
+            else if (Level1.Contains(discipline))
+            {
+                if (discipline.IsChecked)
+                {
+                    _isUpdating = true;
+
+                    foreach (DisciplineViewModel sibling in Level1)
+                    {
+                        if (sibling != discipline)
+                        {
+                            sibling.IsChecked = false;
+                        }
+                    }
+
+                    _isUpdating = false;
+
+                    Level2.Clear();
+                    HasLevel2 = false;
+
+                    foreach (DisciplineViewModel child in discipline.Children)
+                    {
+                        Level2.Add(child);
+                    }
+
+                    HasLevel2 = Level2.Count > 0;
+                }
+                else
+                {
+                    Level2.Clear();
+                    HasLevel2 = false;
+                }
+            }
+            else if (Level2.Contains(discipline))
+            {
+                if (discipline.IsChecked)
+                {
+                    _isUpdating = true;
+
+                    foreach (DisciplineViewModel sibling in Level2)
+                    {
+                        if (sibling != discipline)
+                        {
+                            sibling.IsChecked = false;
+                        }
+                    }
+
+                    _isUpdating = false;
+                }
+            }
         }
 
         private static List<DisciplineViewModel> BuildTree()
         {
-            var telecom = new DisciplineViewModel("TELECOM", 1);
-            telecom.Children.Add(new DisciplineViewModel("CABEAMENTO ESTRUTURADO", 2));
-            telecom.Children.Add(new DisciplineViewModel("CFTV", 2));
-            telecom.Children.Add(new DisciplineViewModel("TELEFONIA", 2));
-            telecom.Children.Add(new DisciplineViewModel("INTERFONIA", 2));
-            telecom.Children.Add(new DisciplineViewModel("CATV", 2));
+            var telecom = new DisciplineViewModel("TELECOM");
+            telecom.Children.Add(new DisciplineViewModel("CABEAMENTO ESTRUTURADO"));
+            telecom.Children.Add(new DisciplineViewModel("CFTV"));
+            telecom.Children.Add(new DisciplineViewModel("TELEFONIA"));
+            telecom.Children.Add(new DisciplineViewModel("INTERFONIA"));
+            telecom.Children.Add(new DisciplineViewModel("CATV"));
 
-            var eletrica = new DisciplineViewModel("ELÉTRICA E AFINS", 0);
+            var eletrica = new DisciplineViewModel("ELÉTRICA E AFINS");
             eletrica.Children.Add(telecom);
-            eletrica.Children.Add(new DisciplineViewModel("SPDA", 1));
-            eletrica.Children.Add(new DisciplineViewModel("ENTRADA DE ENERGIA", 1));
+            eletrica.Children.Add(new DisciplineViewModel("SPDA"));
+            eletrica.Children.Add(new DisciplineViewModel("ENTRADA DE ENERGIA"));
 
             return new List<DisciplineViewModel>
             {
                 eletrica,
-                new DisciplineViewModel("HIDRAULICA", 0),
-                new DisciplineViewModel("PPCI", 0),
-                new DisciplineViewModel("GÁS", 0),
-                new DisciplineViewModel("MECANICA", 0),
-                new DisciplineViewModel("INFRAESTRUTURA", 0),
-                new DisciplineViewModel("ESTRUTURA", 0)
+                new DisciplineViewModel("HIDRAULICA"),
+                new DisciplineViewModel("PPCI"),
+                new DisciplineViewModel("GÁS"),
+                new DisciplineViewModel("MECANICA"),
+                new DisciplineViewModel("INFRAESTRUTURA"),
+                new DisciplineViewModel("ESTRUTURA")
             };
         }
 
-        private static List<DisciplineViewModel> Flatten(List<DisciplineViewModel> nodes)
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
         {
-            var result = new List<DisciplineViewModel>();
-            foreach (DisciplineViewModel node in nodes)
-            {
-                result.Add(node);
-                result.AddRange(Flatten(node.Children));
-            }
-            return result;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
